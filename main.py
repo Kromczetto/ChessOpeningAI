1e3349955faa21405666159dfef05e42a8adc85f
Kromczetto <jk302106@student.polsl.pl>
2024-06-04 15:55:27 +0200

    Some changes, created data feeding and started creating nural network

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, Dense, Dropout
 
+
 #Reading data
 data = pd.read_csv('openings.csv')
 
+features = data[['Num Games', 'Perf Rating', 'Avg Player', 'Player Win %', 'Draw %','Moves']]
+label = data['White_win%']
+
 #Convert to numpy array
-data = data.to_numpy()
+x = features.to_numpy()
+y = label.to_numpy()
 
 #Preparing data to train and test model
 traning = data[0:1500]
@@ -22,15 +27,15 @@ testing_label = testing[:,1]
 
 #Creating model
 model = Sequential()
-model.add(Input(shape=(8,)))
+model.add(Input(shape=(traning_features.shape[1],)))
 model.add(Dense(16, activation='relu'))
 model.add(Dropout(0.2))
-model.add(Dense(16, activation='relu'))
+model.add(Dense(8, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(1, activation='sigmoid'))
 
 #Compile model
-model.compile(optimizer='sgd', loss='bianry_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
 
 #Training model
 model.fit(traning_features, traning_label, epochs=100, validation_data=(testing_features, testing_label))
